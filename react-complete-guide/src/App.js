99d367797cefc0eaeab96bab4a4b9c86ac9da964
reactRaptors <deepthi.yanamandra@verizon.com>
2018-07-03 11:09:38 +0000

    Initial react code commit

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import './App.css';
+import Login from './Login/Login';
+import About from './About/About';
+import Contact from './Contact/Contact';
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+      <div className="App">
+        <div  className="Header">
+          <h1 className="App-title">Raptors Job Search</h1>
+          <ul className ="Menu">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/about">About Us</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+        </div>
+        <div className="Content">
+            <Route exact path="/" component={Login}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
+        </div>
+      </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default App;
